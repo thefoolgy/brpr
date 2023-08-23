3b5d2c4596d074325521f44362c0364326e56470
thefoolgy <99605054+thefoolgy@users.noreply.github.com>
2023-08-23 00:51:27 -0400

    Update train_transformer.py

--- train_transformer.py
+++ train_transformer.py
@@ -4,7 +4,6 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 
 
-# Load your protein sequences
 def load_protein_sequences(filename):
     sequences = []
     for record in SeqIO.parse(filename, "fasta"):
@@ -16,7 +15,7 @@ protein_sequences = load_protein_sequences("dataset/uniprot_sprot.fasta")
 tokenizer = ProteinBertTokenizer(tokens=tokens, vocab_file=None)
 model = BertForMaskedLM.from_pretrained('bert-base-uncased')
 
-# Tokenize sequences
+# tokenize sequences
 inputs = tokenizer(protein_sequences, return_tensors="pt", padding=True, truncation=True)
 
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
@@ -25,7 +24,7 @@ dataloader = DataLoader(inputs, batch_size=32, shuffle=True, collate_fn=data_col
 optimizer = optim.AdamW(model.parameters(), lr=5e-5)
 model.train()
 
-for epoch in range(10):  # Adjust the number of epochs
+for epoch in range(10):  
     for batch in dataloader:
         optimizer.zero_grad()
         outputs = model(**batch)
